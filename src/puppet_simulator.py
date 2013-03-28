ba759ae345cbe7e04b2d20fe7916c1cc5cbfb23f
Jarvis Schultz <jschultz@u.northwestern.edu>
2013-03-28 16:01:24 -0500

    Added markers for all of the strings.  Also added "hook" frames to the urdf.

--- src/puppet_simulator.py
+++ src/puppet_simulator.py
@@ -30,6 +30,9 @@ import rospy
 import tf
 from sensor_msgs.msg import JointState as JS
 import std_srvs.srv as SS
+import visualization_msgs.msg as VM
+import std_msgs.msg as SM
+import geometry_msgs.msg as GM
 import trep
 from trep import tx, ty, tz, rx, ry, rz
 from math import sin, cos, pi
@@ -48,7 +51,64 @@ ARM_LENGTH = 2*0.45 # default arm string lengths
 LEG_LENGTH = 2*0.65 # default leg length strings
 BODY_HEIGHT = 1.50 # default location of the body in the z-axis
 EXCEPTION_COUNT_MAX = 5
+BLACK = (0,0,0,1)
+BLUE = (0.1,0.1,0.5,1)
+GREEN = (0.1,0.5,0.1,1)
+RED = (0.5,0.1,0.1,1)
+WHITE = (1.0,1.0,1.0,1)
 
+
+class StringMarker:
+    def __init__(self, f1, f2, listener, color='black'):
+        self.marker = VM.Marker()
+        self.marker.type = VM.Marker.LINE_LIST
+        self.marker.header.frame_id = "world"
+        self.marker.id = hash(f1+f2)%(2**16)
+        self.marker.scale = GM.Vector3(*(0.01,0.01,0.01))
+        self.set_color(color)
+        self.listener = listener
+        self.f1 = f1
+        self.f2 = f2
+        self.update_points()
+
+    def update_points(self):
+        self.marker.header.stamp = rospy.Time.now()
+        e1, p1 = self.get_point_loc(self.f1)
+        e2, p2 = self.get_point_loc(self.f2)
+        if e1 or e2:
+            p1 = (0,0,0)
+            p2 = (0,0,0)
+        self.marker.points = [
+            GM.Point(*p1),
+            GM.Point(*p2)
+            ]
+
+    def set_color(self, color):
+        if color == 'black':
+            self.marker.color = SM.ColorRGBA(*BLACK)
+        elif color == 'blue':
+            self.marker.color = SM.ColorRGBA(*BLUE)
+        elif color == 'green':
+            self.marker.color = SM.ColorRGBA(*GREEN)
+        elif color == 'red':
+            self.marker.color = SM.ColorRGBA(*RED)
+        elif color == 'white':
+            self.marker.color = SM.ColorRGBA(*WHITE)
+        else:
+            rospy.logwarn("String marker color not recognized!")
+            self.marker.color = SM.ColorRGBA(*BLACK)
+        
+    def get_point_loc(self, frame):
+        try:
+            pos,quat = self.listener.lookupTransform("world", frame,
+                                                     rospy.Time())
+            err = False
+        except (tf.LookupException, tf.ConnectivityException,
+                tf.ExtrapolationException):
+            pos = (0.0, 0.0, 0.0)
+            err = True
+        return err, pos
+    
 
 class PuppetSimulator:
     def __init__(self, sys):
@@ -130,12 +190,22 @@ class PuppetSimulator:
         self.js.header.frame_id = 'world'
         self.update_values()
         self.count_exceptions = 0
-        
+
         # define tf broadcaster and listener
         self.br = tf.TransformBroadcaster()
         self.listener = tf.TransformListener()
+        # add constraint visualization:
+        self.con_vis = [StringMarker('input1', 'left_shoulder_hook', self.listener)]
+        self.con_vis.append(StringMarker('input1', 'right_shoulder_hook', self.listener))
+        self.con_vis.append(StringMarker('input2', 'left_hand_hook', self.listener))
+        self.con_vis.append(StringMarker('input3', 'right_hand_hook', self.listener))
+        if self.legs_bool:
+            self.con_vis.append(StringMarker('input4', 'left_knee_hook', self.listener))
+            self.con_vis.append(StringMarker('input5', 'right_knee_hook', self.listener))
         # define a publisher for the joint states
         self.joint_pub = rospy.Publisher("joint_states", JS)
+        # define a publisher for the constraint Markers
+        self.con_pub = rospy.Publisher("visualization_markers", VM.MarkerArray)
         # define a timer for publishing the frames and tf's
         rospy.Timer(rospy.Duration(1.0/tf_freq), self.send_joint_states)
         # request that we get a service handler:
@@ -143,7 +213,7 @@ class PuppetSimulator:
         rospy.wait_for_service("simulator_reset")
         self.reset_srv_client = rospy.ServiceProxy("simulator_reset", SS.Empty)
         # offer service to reset simulation
-        # self.reset_srv_provider = 
+        # self.reset_srv_provider =
         # define a timer for integrating the state of the VI
         rospy.loginfo("Starting integration...")
         rospy.Timer(rospy.Duration(DT), self.integrate_vi)
@@ -370,6 +440,14 @@ class PuppetSimulator:
             self.br.sendTransform(point, quat,
                                   tnow,
                                   'input5', 'world')
+        # update the constraint visualization:
+        mlist = []
+        for c in self.con_vis:
+            c.update_points()
+            mlist.append(c.marker)
+        ma = VM.MarkerArray()
+        ma.markers = mlist
+        self.con_pub.publish(mlist)
 
 
 
